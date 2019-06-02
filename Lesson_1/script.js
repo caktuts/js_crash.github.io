2ff67cb718aabcec76f439809f8097bc7990ca45
Anzor <anzortamaev@yahoo.com>
2019-06-02 10:59:26 +0300

    Это мой первый коммит

--- Lesson_1/script.js
+++ Lesson_1/script.js
@@ -1,46 +1,27 @@
-'use strict';
+var money = prompt("Ваш бюджет на месяц?", ''),
+	time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
-console.log(4/0);
-console.log('string'*9);
-let something;
-console.log(something);
+var appData = {
+    budget: money,
+    timeData: time,
+	expenses: {
+        cost:price
+    },
+	optionalExpenses: {},
+	income: [],
+	
+	savings: false
+};
 
-let person = {
-    name: "John",
-    age: 25,
-    isMarried: false
-}
 
-console.log(person["name"]);
 
-let arr = ['plum.png','oreange.jpg','apple.bmp'];
+var cost = prompt("Введите обязательную статью расходов в этом месяце",""),
+    cost2 = prompt("Введите обязательную статью расходов в этом месяце","");
+var price = prompt("Во сколько это обойдется?",""),
+    price2 = prompt("Во сколько это обойдется?","");
+    
+    alert(appData.budget/30);
+    
+    price = price2;
+    cost = cost2;
 
-console.log(arr[0]);
-
-//alert("Hello,world!");
-
-// let answer = confirm("Are you here?");
-
-// console.log(answer);
-
-// let answer = +prompt("Есть ли вам 18?", "Да");
-
-// console.log(typeof(answer));
-
-// console.log("arr" + "- object");
-// console.log(4 +  +"- object");
-
-let incr = 10,
-    decr = 10;
-
-console.log(incr++);
-console.log(decr--);
-
-console.log(5%2);
-
-console.log("2" == 2);
-
-let isChecked = false,
-    isClose = false;
-
-console.log(isChecked || !isClose);
